4679424985ee2bd3df30f3f953f47719c9476c64
alejomendez1019 <alejomendez1019@gmail.com>
2023-02-01 21:03:15 -0500

    fixed createBookingCheckout function

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -56,7 +56,8 @@ exports.getCheckoutSession = catchAsync(async (req, res, next) => {
 
 const createBookingCheckout = async (session) => {
   const tour = session.client_reference_id;
-  const user = (await User.findOne({ email: session.customer_email })).id;
+  const user = (await User.findOne({ email: session.customer_email }))._id;
+  console.log('USER: ', user);
   const price = session.display_items[0].amount / 100;
   await Booking.create({ tour, user, price });
 };
@@ -71,7 +72,6 @@ exports.webhookCheckout = (req, res, next) => {
     return res.status(400).send(`Webhook error: ${err.message}`);
   }
   console.log('TIPO DE EVENTO: ', event.type);
-  console.log('EVENTO objeto: ', event.data.object);
   if (event.type === 'checkout.session.completed') createBookingCheckout(event.data.object);
 
   res.status(200).json({ received: true });
